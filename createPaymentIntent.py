7851dc3bd0a3d1dc9f0b929f452b0fdf5999cf95
Obaid4587 <85950471+Obaid4587@users.noreply.github.com>
2021-12-13 17:01:15 +0500

    Update createPaymentIntent.py

--- createPaymentIntent.py
+++ createPaymentIntent.py
@@ -9,7 +9,7 @@ def lambda_handler(data, context):
   email = data['email']
   payment_method_id = data['payment_method_id']
   
-  stripe.api_key = 'sk_test_...' #Your test/live secret key
+  stripe.api_key = 'pk_test_51IYZ9kBacowHdHsgZqK8SpJeHum3mmXSupwIbZ4693MSMmGlgsqur6BrkhtdGCHq78SSeF5o73wrIxXcavr8rAKw00BHnxQmqm' #Your test/live secret key
   
   payment_intent = stripe.PaymentIntent.create(
     payment_method_types=['card'],
@@ -17,7 +17,7 @@ def lambda_handler(data, context):
     amount=1000,
     application_fee_amount=140,
     currency='eur',
-    stripe_account='acct_1G...',#connected account ID
+    stripe_account='acct_1K5qyBPjANuxJ0xT',#connected account ID
     receipt_email=email,
     confirm=True
   )
